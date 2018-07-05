5dbedb26452fd4b667b8c2f716b98a21e2343a9b
Otto Negron <ottonegron@hotmail.com>
2018-07-04 20:36:50 -0400

    Arrays: Two Sum and Spiral Matrix

--- Algorithms/SpiralMatrix.cs
+++ Algorithms/SpiralMatrix.cs
@@ -8,9 +8,45 @@ namespace Arrays
 {
     public class SpiralMatrix
     {
-        //public IList<int> SpiralOrder(int[,] matrix)
-        //{
+        public IList<int> SpiralOrder(int[,] matrix)
+        {
+            IList<int> result = new List<int>();
 
-        //}
+            if (matrix.GetLength(1) == 0) return result; // there are no rows
+
+            int leftBound = 0;
+            int rightBound = matrix.GetLength(0); // #columns
+            int upperBound = 0;
+            int lowerBound = matrix.GetLength(1); // #rows
+
+            while (leftBound <= rightBound && upperBound <= lowerBound)
+            {
+                for (int col = leftBound; col <= rightBound; col++)
+                {
+                    result.Add(matrix[upperBound, col]); // row THEN column
+                }
+                upperBound++;
+
+                for (int row = upperBound; row <= lowerBound; row++)
+                {
+                    result.Add(matrix[row, rightBound]);
+                }
+                rightBound--;
+
+                for (int col = rightBound; upperBound <= lowerBound && col >= leftBound; col--)
+                {
+                    result.Add(matrix[lowerBound, col]);
+                }
+                lowerBound--;
+
+                for (int row = lowerBound; leftBound <= rightBound && row >= upperBound; row--)
+                {
+                    result.Add(matrix[row, leftBound]); 
+                }
+                leftBound++;
+            }
+
+            return result;
+        }
     }
 }
---
+++ Algorithms/TwoSum.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class TwoSum
+    {
+        public int[] FindTwoSum(int[] nums, int target)
+        {
+            List<int> result = new List<int>();
+
+            if (nums == null || nums.Length == 0) return result.ToArray();
+
+            // key is the number, value is the index of the last occurrence of the number in the input array
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int found;
+                if(!dic.TryGetValue(nums[i], out found))
+                {
+                    dic.Add(nums[i], i);
+                }
+                else
+                {
+                    dic[nums[i]] = i;
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int val = nums[i];
+
+                int n = target - val;
+
+                // int index = Array.IndexOf(nums, n); // this has O(n) runtime
+                int index = -1;
+                dic.TryGetValue(n, out index);
+
+                if(index >= 0 && i != index) // make sure we're not using the same number
+                {
+                    result.Add(i);
+                    result.Add(index);
+                    break;
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
