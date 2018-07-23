dd1b825c30d8e6b8f829c7f9a9c829c88192346c
Omar Negron <omarnegron92@hotmail.com>
2018-07-22 22:09:16 -0400

    Arrays: Median of Two Sorted Arrays + Container With Most Water

---
+++ Algorithms/ArrayPartitionI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class ArrayPartitionI
+    {
+        /// <summary>
+        /// Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible. 
+        /// n is a positive integer, which is in the range of [1, 10000].
+        /// All the integers in the array will be in the range of[-10000, 10000].
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int ArrayPairSum(int[] nums)
+        {
+            return 0;
+
+            /* My N Log N brute force solution
+            int sum = 0;
+
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i % 2 == 0) sum += nums[i];
+            }
+
+            return sum;
+            */
+        }
+    }
+}
---
+++ Algorithms/ContainerWithMostWater.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            int i = 0;
+            int j = height.Length - 1;
+
+            int h = 0, l = 0;
+            int maxArea = 0;
+
+            while (i < j)
+            {
+                l = j - i;
+
+                // lowest height takes precedence
+                if (height[i] < height[j])
+                {
+                    h = height[i];
+                    i++;
+                }
+                else
+                {
+                    h = height[j];
+                    j--;
+                }
+
+                if (h * l > maxArea)
+                    maxArea = h * l;
+            }
+
+            return maxArea;
+        }
+    }
+}
