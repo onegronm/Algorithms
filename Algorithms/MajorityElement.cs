aabc834a12fc8a0b1c2226e0e1ce2e575d84a13e
Omar Negron <omarnegron92@hotmail.com>
2018-07-29 17:41:10 -0400

    Arrays: Majority Element

---
+++ Algorithms/MajorityElement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class MajorityElement
+    {
+        public int FindMajorityElement(int[] nums)
+        {
+            // first build a number frequency table
+            Dictionary<int, int> frequency = new Dictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                int count = 0;
+                if (frequency.TryGetValue(num, out count))
+                {
+                    frequency[num]++;
+                }
+                else
+                {
+                    frequency.Add(num, 1);
+                }
+            }
+
+            // second find out which frequency is greater than n/2 and return it
+            int majorityElement = nums[0];
+
+            foreach (int k in frequency.Keys)
+            {
+                int count = 0;
+
+                frequency.TryGetValue(k, out count);
+
+                if (count > nums.Length / 2)
+                    majorityElement = k;
+            }
+
+            return majorityElement;
+        }
+    }
+}
