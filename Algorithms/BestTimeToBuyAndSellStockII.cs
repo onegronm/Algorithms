a07abf07725a6c7d1d592dd871d45592a9b67770
Omar Negron <omarnegron92@hotmail.com>
2018-07-29 17:06:25 -0400

    Arrays: Best Time to Buy and Sell Stock II

---
+++ Algorithms/BestTimeToBuyAndSellStockII.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class BestTimeToBuyAndSellStockII
+    {
+        // Time complexity: O(n). Single pass.
+        // Space complexity: O(1). Constant space needed.
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+
+            if (prices == null || prices.Length < 2)
+                return maxProfit;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if(prices[i] > prices[i-1])
+                {
+                    maxProfit += prices[i] - prices[i - 1];
+                }
+            }
+
+            return maxProfit;
+        }
+    }
+}
--- Algorithms/MaximumSubarray.cs
+++ Algorithms/MaximumSubarray.cs
@@ -8,73 +8,97 @@ namespace Arrays
 {
     public class MaximumSubarray
     {
-        public static void Main(string[] args)
-        {
-            MaximumSubarray program = new MaximumSubarray();
+        //public static void Main(string[] args)
+        //{
+        //    MaximumSubarray program = new MaximumSubarray();
 
-            int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-            int[] nums2 = { -2, -3, -1 };
-            int[] nums3 = { -1, -2 };
-            int[] nums4 = { -2, -1 };
-            int[] nums5 = { -1 };
-            int[] nums6 = { 1, 2, -1, -2, 2, 1, -2, 1, 4, -5, 4 };
+        //    int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        //    int[] nums2 = { -2, -3, -1 };
+        //    int[] nums3 = { -1, -2 };
+        //    int[] nums4 = { -2, -1 };
+        //    int[] nums5 = { -1 };
+        //    int[] nums6 = { 1, 2, -1, -2, 2, 1, -2, 1, 4, -5, 4 };
 
-            int result = program.MaxSubArray(nums);
-            int result2 = program.MaxSubArray(nums2);
-            int result3 = program.MaxSubArray(nums3);
-            int result4 = program.MaxSubArray(nums4);
-            int result5 = program.MaxSubArray(nums5);
-            int result6 = program.MaxSubArray(nums6);
+        //    int result = program.MaxSubArray(nums);
+        //    int result2 = program.MaxSubArray(nums2);
+        //    int result3 = program.MaxSubArray(nums3);
+        //    int result4 = program.MaxSubArray(nums4);
+        //    int result5 = program.MaxSubArray(nums5);
+        //    int result6 = program.MaxSubArray(nums6);
 
-            Console.WriteLine(result);
-            Console.Read();
-        }
+        //    Console.WriteLine(result);
+        //    Console.Read();
+        //}
 
+        // O(n) solution using Dynamic Programming
         public int MaxSubArray(int[] nums)
         {
-            int sum = 0;
+            int maxSubArray = nums[0];
 
             if (nums == null || nums.Length == 0)
-                return sum;
+                return maxSubArray;
 
-            // 1. Calculate the sum of all elements
-            foreach (int num in nums)
-                sum += num;
-
-            int max = sum;
-            
-            int i = 0, j = nums.Length - 1;
-
-            int sumOfDiscardedNumbers = 0;
-
-            while (i < j)
+            for (int i = 1; i < nums.Length; i++)
             {
-                int leftArraySum = sum - nums[j] - sumOfDiscardedNumbers;
-                int rightArraySum = sum - nums[i] - sumOfDiscardedNumbers;                
-
-                if (leftArraySum > max)
+                if (nums[i] + nums[i - 1] > nums[i])
                 {
-                    max = leftArraySum;
+                    nums[i] += nums[i - 1];
                 }
 
-                if (rightArraySum > max)
+                if(nums[i] > maxSubArray)
                 {
-                    max = rightArraySum;
+                    maxSubArray = nums[i];
                 }
+            }
 
-                if (rightArraySum > leftArraySum)
-                {
-                    sumOfDiscardedNumbers += nums[i];
-                    i++;
-                }
-                else
-                {
-                    sumOfDiscardedNumbers += nums[j];
-                    j--;
-                }                    
-            } 
-
-            return max;
+            return maxSubArray;
         }
+
+        //public int MaxSubArray(int[] nums)
+        //{
+        //    int sum = 0;
+
+        //    if (nums == null || nums.Length == 0)
+        //        return sum;
+
+        //    // 1. Calculate the sum of all elements
+        //    foreach (int num in nums)
+        //        sum += num;
+
+        //    int max = sum;
+
+        //    int i = 0, j = nums.Length - 1;
+
+        //    int sumOfDiscardedNumbers = 0;
+
+        //    while (i < j)
+        //    {
+        //        int leftArraySum = sum - nums[j] - sumOfDiscardedNumbers;
+        //        int rightArraySum = sum - nums[i] - sumOfDiscardedNumbers;                
+
+        //        if (leftArraySum > max)
+        //        {
+        //            max = leftArraySum;
+        //        }
+
+        //        if (rightArraySum > max)
+        //        {
+        //            max = rightArraySum;
+        //        }
+
+        //        if (rightArraySum > leftArraySum)
+        //        {
+        //            sumOfDiscardedNumbers += nums[i];
+        //            i++;
+        //        }
+        //        else
+        //        {
+        //            sumOfDiscardedNumbers += nums[j];
+        //            j--;
+        //        }                    
+        //    } 
+
+        //    return max;
+        //}
     }
 }
