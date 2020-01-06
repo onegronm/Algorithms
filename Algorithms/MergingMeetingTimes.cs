72dbe75daad3c59479de24abb4982bd9daf3bddb
Omar Negron Montero <Omar.NegronMontero@crowe.com>
2020-01-05 22:15:27 -0500

    MergingMeetingTimes continued

--- Algorithms/MergingMeetingTimes.cs
+++ Algorithms/MergingMeetingTimes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Arrays
 {
@@ -8,7 +9,7 @@ namespace Arrays
     Your company built an in-house calendar tool called HiCal. 
     You want to add a feature to see the times in a day when everyone is available.
     To do this, you’ll need to know when any team is having a meeting. 
-    In HiCal, a meeting is stored as a tuple ↴ of integers (start_time, end_time).
+    In HiCal, a meeting is stored as a tuple of integers (start_time, end_time).
     These integers represent the number of 30-minute blocks past 9:00am.
 
     (2, 3)  # Meeting from 10:00 – 10:30 am
@@ -36,13 +37,132 @@ namespace Arrays
          
     */
 
-    public class MergingMeetingTimes
+    /// <summary>
+    /// Complexity: O(nlgn) time and O(n)O(n) space.
+    /// Even though we only walk through our list of meetings once to merge them, we sort all the meetings first, giving us a runtime of O(n\lg{n})O(nlgn). It's worth noting that if our input were sorted, we could skip the sort and do this in O(n)O(n) time!
+    /// We create a new list of merged meeting times. In the worst case, none of the meetings overlap, giving us a list identical to the input list. Thus we have a worst-case space cost of O(n)O(n).
+    /// </summary>
+    public static class MergingMeetingTimes
     {
-        public List<Tuple<int, int>> Merge_ranges(List<Tuple<int, int>> meetingTimeRanges)
+        public static void Merge_ranges(List<Meeting> meetingTimeRanges)
         {
-            List<Tuple<int, int>> condensedTimeRanges = new List<Tuple<int, int>>();
+            List<Meeting> mergedMeetings = new List<Meeting>();
+
+            /*
+             
+            Breakdown
+
+            What if we had two ranges?
+
+            [(1, 3), (2, 4)]
+
+            These meetings clearly overlap, so we should merge them to give:
+
+            [(1, 4)]
+
+            But how did we know that these meetings overlap?
+
+            We could tell the meetings overlapped because the end time of the first 
+            one was after the start time of the second one! But our ideas of "first"
+            and "second" are important here—this only works after we ensure that we 
+            treat the meeting that starts earlier as the "first" one.
+
+            How would we formalize this as an algorithm? Be sure to consider these edge cases:
+
+            1. The end time of the first meeting and the start time of the second meeting are equal. For example: [(1, 2), (2, 3)]
+                These do not overlap
+            2. The second meeting ends before the first meeting ends. For example: [(1, 5), (2, 3)]
+                The bigger range takes precedence
+
+            Here's a formal algorithm:
+
+            1. We treat the meeting with earlier start time as "first," and the other as "second."
+            2. If the end time of the first meeting is equal to or greater than the start time of the second meeting, we merge the two meetings into one time range. The resulting time range's start time is the first meeting's start, and its end time is the later of the two meetings' end times.
+            3. Else, we leave them separate.
+
+            So, we could compare every meeting to every other meeting in this way, merging them or leaving them separate.
+
+            Comparing all pairs of meetings would take O(n^2) time. We can do better!
             
-            return condensedTimeRanges;
+            If we're going to beat O(n^2) time, maybe we're going to get O(n)O(n) time? Is there a way to do this in one pass?
+             
+            It'd be great if, for each meeting, we could just try to merge it with the next meeting. But that's definitely not sufficient, because the ordering of our meetings is random. There might be a non-next meeting that the current meeting could be merged with.
+             
+            (so far sorting is not in scope)
+
+            What if we sorted our list of meetings by start time? (!) Then any meetings that could be merged would always be adjacent!
+
+            So we could sort our meetings, then walk through the sorted list and see if each meeting can be merged with the one after it.
+
+            Sorting takes O(n\lg{n})O(nlgn) time in the worst case. If we can then do the merging in one pass, that's another O(n) time, for O(n lgn) overall. That's not as good as O(n)O(n), but it's better than O(n^2)
+            
+            */
+
+
+            // First, we SORT our input list of meetings by start time so any meetings that might need to be merged are now next to each other.
+            var sortedMeetings = SortList(meetingTimeRanges);
+
+            // Initialize mergedMeetings with th earliest meeting
+            mergedMeetings.Add(sortedMeetings[0]);
+
+            // merging is done in one pass and takes n time
+            foreach (var currentMeeting in sortedMeetings)
+            {
+                var lastMergedMeeting = mergedMeetings.Last();
+
+                if (currentMeeting.StartTime <= lastMergedMeeting.EndTime)
+                {
+                    // If the current meeting overlaps with the last merged meeting,
+                    // use the later end time of the two
+                    lastMergedMeeting.EndTime =
+                        Math.Max(lastMergedMeeting.EndTime, currentMeeting.EndTime);
+                }
+                else
+                {
+                    // Add the current meeting since it doesn't overlap
+                    mergedMeetings.Add(currentMeeting);
+                }
+            }
+
+            // print results
+            Console.WriteLine("Merging Meeting Times:");
+            foreach (var item in mergedMeetings)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            // runtime = O(n + n log n) = O(n lgn)
+        }
+
+        private static List<Meeting> SortList(List<Meeting> meetingRanges)
+        {
+            // sorting with order by takes n lg n time (quickSort)
+
+            // make a copy so we don't destroy the input
+            return meetingRanges.Select(m => new Meeting(m.StartTime, m.EndTime))
+                .OrderBy(m => m.StartTime).ToList();
+        }
+    }
+
+    /// <summary>
+    /// Create an object to represent the tuple
+    /// </summary>
+    public class Meeting
+    {
+        public int StartTime { get; set; }
+
+        public int EndTime { get; set; }
+
+        public Meeting(int startTime, int endTime)
+        {
+            // Number of 30 min blocks past 9:00 am
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        public override string ToString()
+        {
+            return $"({StartTime}, {EndTime})";
         }
     }
 }
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -34,9 +34,67 @@ namespace Arrays
 
             arraysAndStrings.Run();
 
-            MergingMeetingTimes mergingMeetingTimes = new MergingMeetingTimes();
+            MergingMeetingTimes.Merge_ranges(new List<Meeting> {
+                new Meeting(0,1), new Meeting(3, 5), new Meeting(4, 8), new Meeting(9, 10)
+            });
 
             Console.Read();
         }
     }
 }
+
+
+
+
+/*
+
+    .OrderBy()
+
+    This method is implemented by using deferred execution. The immediate return value is an
+    object that stores all the information that is required to perform the action. The query
+    represented by this method is not executed until the object is enumerated either by 
+    calling its GetEnumerator method directly or by using foreach in Visual C# or For Each in Visual Basic.
+
+    sort operation stays the typical for QuickSort O(N*logN) average / O(N2) worst case
+    Mathematical analysis of quicksort shows that, on average, the algorithm takes O(n log n) 
+    comparisons to sort n items. In the worst case, it makes O(n2) comparisons, though this behavior is RARE.
+    
+    .Add() 
+    
+    operation runs in O(1) time. List is a dynamic array with a number of elements
+    that are needed before resizing is required (capacity). If Count exceeds Capacity while
+    adding elements, the capacity is increased by automatically reallocating the internal 
+    array before copying the old elements and adding the new elements.
+    
+    If Count is less than Capacity, this method is an O(1) operation. 
+    f the capacity needs to be increased to accommodate the new element, this method becomes an O(n) operation, where n is Count.
+
+    .Capacity 
+    
+    retrieving the value of this property is an O(1) operation; setting the property is an O(n) operation, where n is the new capacity.
+
+    .Distinct() 
+    
+    operation runs in O(n) time
+
+    .Where() 
+    
+    operation runs in O(n)
+
+    .Contains() depends on the underlying data structure – in the case of a List, O(n)
+
+    .ToList()
+    it is an O(n) operation though it will likely only require attention in performance critical operations.
+    The ToList() operation will use the List(IEnumerable<T> collection) constructor. This constructor must make a copy of the array (more generally IEnumerable<T>)
+    A List<T> also uses a construct called a dynamic array which needs to be resized on demand, this resize event copies the contents of an old array to the new array. So it starts off small and increases in size if required.
+    This is the difference between the Capacity and Count attributes on List<T>. 
+    Capacity refers to the size of the array behind the scenes, 
+    Count is the number of items in the List<T> which is always <= Capacity. 
+    So when an item is added to the list, increasing it past Capacity, the size of the
+    List<T> is doubled and the array is copied.
+
+    .Select()
+
+    .FirstOrDefault()
+    https://www.nerdhold.com/coder/2014/11/25/linq-and-time-complexity/
+ */
