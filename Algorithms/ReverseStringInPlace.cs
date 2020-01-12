28447a512c4803995bc0297cf328d934f94788d1
Omar Negron <omarnegron92@hotmail.com>
2020-01-11 23:47:52 -0500

    ReverseStringInPlace + Unit Tests

---
+++ Algorithms/ReverseStringInPlace.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    /// <summary>
+    /// Write a method that takes an array of characters and reverses the letters in place.
+    /// </summary>
+    public static class ReverseStringInPlace
+    {
+        /// <summary>
+        /// Time complexity: O(n)
+        /// Space complexity: constant (no additional space is needed)
+        /// </summary>
+        /// <param name="arrayOfChar"></param>
+        public static void Reverse(char[] arrayOfChar)
+        {
+            int lastIndex = arrayOfChar.Length - 1;
+
+            for (int i = 0; lastIndex > i; i++)
+            {
+                // save the value of the last element
+                var last = arrayOfChar[lastIndex];
+
+                // replace the last element with the value of the "first" element
+                arrayOfChar[lastIndex] = arrayOfChar[i];
+
+                // replace the "first" element with the value of the last element
+                arrayOfChar[i] = last;
+
+                // move lastIndex down one position
+                lastIndex--;
+            }
+        }
+    }
+}
