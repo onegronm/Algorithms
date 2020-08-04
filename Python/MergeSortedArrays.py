110ec78f380b1a7dd60c44b0d187796e7f951cf8
Omar Negron <omarnegron92@hotmail.com>
2020-08-03 21:59:03 -0400

    merging sorted arrays

--- Python/MergeSortedArrays.py
+++ Python/MergeSortedArrays.py
@@ -23,8 +23,41 @@ alices_list = [1, 5, 8, 12, 14, 19]
 def merge_sorted_lists(arr1, arr2):
 	return sorted(arr1 + arr2);
 
-
-# the input lists are themselves already sorted. How can we save time by using this fact?
-
 # Prints [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
 print(merge_sorted_lists(my_list, alices_list))
+
+# the input lists are themselves already sorted. How can we save time by using this fact?
+def merge_lists(my_list, alice_list):
+	merged_list_size = len(my_list) + len(alice_list)
+	merged_list = [None] * merged_list_size # initialize list elements with null (none) 
+
+	current_index_alices = 0
+	current_index_mine = 0
+	current_index_merged = 0
+
+	# edge cases:
+	# 1. One or both of our input lists is 0 elements or 1 element
+	# 2. One of our input lists is longer than the other.
+	# 3. One of our lists runs out of elements before we're done merging.
+
+	while current_index_merged < merged_list_size:
+		first_unmerged_alices = alice_list[current_index_alices]
+		first_unmerged_mine = my_list[current_index_mine]
+
+		if first_unmerged_mine < first_unmerged_alices:
+			# next comes from my list
+			merged_list[current_index_merged] = first_unmerged_mine
+			current_index_mine += 1
+		else:
+			# next comes from Alice's list
+			merged_list[current_index_merged] = first_unmerged_alices
+			current_index_alices += 1
+
+		current_index_merged += 1
+
+	return merged_list
+
+
+print(merge_lists(my_list, alices_list))
+
+
