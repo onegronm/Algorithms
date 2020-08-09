aee244f75fb985849eaa3e2cbe96711700956d68
Omar Negron <omarnegron92@hotmail.com>
2020-08-09 18:00:27 -0400

    moved files to folder

--- Python/Arrays/FizzBuzz.py
+++ Python/Arrays/FizzBuzz.py
@@ -1,5 +1,6 @@
 
-# The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops BEFORE a specified number.
+# The range() function returns a sequence of numbers, starting from 0 by default, 
+# and increments by 1 (by default), and stops BEFORE a specified number.
 for i in range(1, 101):
 	if i%3 == 0 and i%5 == 0:
 		print("FizzBuzz")
