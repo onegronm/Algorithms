71ddaa1d0b784609d507efd2b508d73b9a33d314
Omar Negron <omarnegron92@hotmail.com>
2020-08-09 15:16:59 -0400

    fizz buzz

--- Python/FizzBuzz.py
+++ Python/FizzBuzz.py
@@ -1,12 +1,12 @@
 
-
-for i in range(1, 100):
+# The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops BEFORE a specified number.
+for i in range(1, 101):
 	if i%3 == 0 and i%5 == 0:
-		print(str(i) + " FizzBuzz")
+		print("FizzBuzz")
 	elif i%3 == 0:
-		print(str(i) + " Fizz")
+		print("Fizz")
 	elif i%5 == 0:
-		print(str(i) + " Buzz")
+		print("Buzz")
 	else:
 		print(i)
 	
