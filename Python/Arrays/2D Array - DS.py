a793f95b349925e17b5f677106e740d784142fe0
Omar Negron <omarnegron92@hotmail.com>
2020-08-12 00:36:45 -0400

    2D array simpler

--- Python/Arrays/2D Array - DS.py
+++ Python/Arrays/2D Array - DS.py
@@ -1,6 +1,7 @@
 # https://www.hackerrank.com/challenges/2d-array/problem
 
 # time complexity O(n) x O(m) = O(n * m) cartesian product. Could grow quadratically.
+# O(1) space complexity
 def hourGlassSum(arr):
 
 	if len(arr) == 0 or len(arr[0]) == 0:
@@ -50,6 +51,18 @@ def hourGlassSum(arr):
 	return highestSum
 
 
+def hourGlassSumSimpler(arr):
+	sums = []
+
+	for i in range(len(arr)-2):
+		for j in range(len(arr)-2):
+			sums.append(arr[i][j] + arr[i][j+1] + arr[i][j+2]
+			   + arr[i+1][j+1]
+			   + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2])
+
+	return max(sums)
+
+
 arr = [
 	[1, 1, 1, 0, 0, 0],
 	[0, 1, 0, 0, 0, 0],
@@ -61,6 +74,4 @@ arr = [
 
 print(hourGlassSum(arr))
 
-arr = []
-
-print(hourGlassSum(arr))
+print(hourGlassSumSimpler(arr))
