8880a75d190b4df2e35c1571ea0ecaa4ff38472d
Omar Negron <omarnegron92@hotmail.com>
2020-09-15 12:59:02 -0400

    level order iterative

--- Python/Trees and Graphs/LeetCode/BinaryTreeLevelOrder.py
+++ Python/Trees and Graphs/LeetCode/BinaryTreeLevelOrder.py
@@ -25,4 +25,43 @@ def levelOrder(self, root):
 	helper(root, 0)
 	return levels
 
+# Let's keep nodes of each tree level in the queue structure, which typically orders elements in a FIFO (first-in-first-out) manner.
+#  Queue structure would be an overkill since it's designed for a safe exchange between multiple threads and hence requires locking which leads to a performance loss.
+# time O(n)
+# space O(n)
+# We have to maintain a queue to help us to do the traversal. And the size of the queue will be at most N because each node will be pushed into the queue exactly once. Therefore, the space complexity of level-order traversal is also O(N).
+def levelOrderIterative(self, root):
+
+	levels = []
+	if not root:
+		return levels
+
+	level = 0
+	queue = deque([root,])
+
+	while queue:
+
+		# start at the current level
+		levels.append([])
+
+		# number of elements in the current level
+		level_length = len(queue)
+
+		for i in range(level_length):
+			node = queue.popleft() # Remove and return an element from the left side of the deque. If no elements are present, raises an IndexError.
+			# fulfill the current level
+			levels[level].append(node.val)
+
+			# add child nodes of the current level
+			# in the queue for the next level
+			if node.left:
+				queue.append(node.left)
+			if node.right:
+				queue.append(node.right)
+
+		# go to the next level
+		level += 1
+
+	return levels
+
 
