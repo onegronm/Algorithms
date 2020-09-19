fa4c7b2c30b95cb9c12fc0341dbc47884056e148
Omar Negron <omarnegron92@hotmail.com>
2020-09-19 16:48:47 -0400

    isSymmetric 2 (faster)

--- Python/Trees and Graphs/LeetCode/Recursion/isSymmetric.py
+++ Python/Trees and Graphs/LeetCode/Recursion/isSymmetric.py
@@ -58,16 +58,19 @@ def isSymmetricRecursive(self,root):
 
     return True
 
-# recursive option 2
-#bool isSymmetric(TreeNode* root) {
-#        if (!root) return true;
-#        return isSymmetric(root->left, root->right);
-#    }
-    
-#    bool isSymmetric(TreeNode* t1, TreeNode* t2){
-#        if (!t1 && !t2) return true;
-#        if (!t1 || !t2) return false;
-#        return t1->val == t2->val
-#            && isSymmetric(t1->left, t2->right)
-#            && isSymmetric(t1->right, t2->left);
-#    }
+
+def isSymmetric2(self, root):
+
+    if not root:
+        return True;
+
+    def isSymmetric(left, right):
+        if not left and not right:
+            return True
+
+        if not left or not right:
+            return False
+
+        return left.val == right.val and isSymmetric(left.left, right.right) and isSymmetric(left.right, right.left)
+
+    return isSymmetric(root.left, root.right)
