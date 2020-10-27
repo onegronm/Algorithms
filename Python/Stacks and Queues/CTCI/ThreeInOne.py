264bc767704d9520333cb02563b1c818fa08dbf9
Omar Negron <omarnegron92@hotmail.com>
2020-10-26 21:47:31 -0400

    three in one fixed division

--- Python/Stacks and Queues/CTCI/ThreeInOne.py
+++ Python/Stacks and Queues/CTCI/ThreeInOne.py
@@ -1,26 +1,44 @@
 # describe how you could use a single array to implement three stacks
+# similar to the N stack problem
 
 # hints:
 # divide array in 3 parts for each stack
 # shift stacks around
 # circular array
 
-# input: number of stacks, stack default size
-# method: shift stacks around (circular array) until array is full
-# when full, create new array double the size
-# assume all stacks are of the same type
+# Approach 1: fixed division
+class ThreeInOneFixedDivision:
 
-class ThreeInOne:
+	def __init__(self, stackSize):
 
-	def __init__(self, numberOfStacks, defaultStackSize):
+		self.numberOfStacks = 3
+		self.stackCapacity = stackSize
+		self.sizes = [0] * 3
+		self.values = [0] * stackSize * self.numberOfStacks		
 		
-		self.stack = [None] * defaultStackSize * numberOfStacks
-		x = 1
+	def push(self, stack, value):
+		if self.isFull(stack):
+			raise Exception("Stack is full.")
 
-stacks = ThreeInOne(3,3)
-#stacks.push(0,12)
-#stacks.push(0,2)
-#stacks.push(1,2)
+		self.sizes[stack] += 1 # increment sizes array
+		self.values[self.indexOfTop(stack)] += value
 
+	def isFull(self, stack):
+		return self.sizes[stack] == self.stackCapacity
 
-	
+	def indexOfTop(self, stack):
+		offSet = stack * self.stackCapacity
+		size = self.sizes[stack]
+		return offSet + size - 1
+
+stacks = ThreeInOneFixedDivision(3)
+stacks.push(0,12)
+stacks.push(0,2)
+stacks.push(0,100)
+stacks.push(1,38)
+stacks.push(1,77)
+stacks.push(1,42)
+stacks.push(2,54)
+stacks.push(2,81)
+stacks.push(2,63)
+print(stacks)
