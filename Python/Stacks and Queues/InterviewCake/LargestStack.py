042b9beefc3878719cf8206524cc73834344d7b7
Omar Negron <omarnegron92@hotmail.com>
2020-11-10 23:09:09 -0500

    largest stack

--- Python/Stacks and Queues/InterviewCake/LargestStack.py
+++ Python/Stacks and Queues/InterviewCake/LargestStack.py
@@ -9,13 +9,16 @@ class MaxStack(object):
         """Initialize an empty stack"""
         self.items = []
         self.maxValuesStack = []
+        self.maxIndex = {}
 
     def push(self, item):
         """Push a new item onto the stack"""
         self.items.append(item)
 
-        if len(self.maxValuesStack) == 0 or item > self.maxValuesStack[-1]:
+        if len(self.maxValuesStack) == 0 or item >= self.maxValuesStack[-1]:
             self.maxValuesStack.append(item)
+            # save the index of the maximum
+            self.maxIndex[item] = len(self.items) - 1
 
     def pop(self):
         """Remove and return the last item"""
@@ -41,8 +44,11 @@ class MaxStack(object):
     def peekMax(self) -> int:
         """
         """
-        if not self.maxValuesStack:
+        if not self.maxValuesStack and not self.items:
             return None
+
+        if not self.maxValuesStack:
+            return self.items[-1]
 
         return self.maxValuesStack[-1]
         
@@ -50,12 +56,49 @@ class MaxStack(object):
     def popMax(self) -> int:
         """
         """
-        if not self.maxValuesStack:
+        if not self.maxValuesStack and not self.items:
             return None
 
-        val = self.maxValuesStack.pop()
-        self.items.remove(val)
+        if self.maxValuesStack:
+            val = self.maxValuesStack.pop()
+        else:
+            val = self.items.pop()
+
+        if val in self.maxIndex.keys():
+            self.items.pop(self.maxIndex[val])
+        else:
+            self.items.pop()
 
         return val
 
+["MaxStack","push","push","push","top","popMax","top","peekMax","pop","top"]
+[[],[5],[1],[5],[],[],[],[],[],[]]
+
+stack = MaxStack();
+stack.push(5); 
+stack.push(1);
+stack.push(5);
+print(stack.top()); #-> 5
+print(stack.popMax()); #-> 5
+print(stack.top()); #-> 1
+print(stack.peekMax()); #-> 5
+print(stack.pop()); #-> 1
+print(stack.top()); #-> 5
+
+["MaxStack","push","push","popMax","peekMax"]
+[[],[5],[1],[],[]]
+# Expected: [null,null,null,5,1]
+
+stack = MaxStack();
+stack.push(5); 
+stack.push(1);
+print(stack.popMax()); #-> 5
+print(stack.peekMax()); #-> 5
+
+
+stack = MaxStack();
+stack.push(5); 
+stack.push(5);
+print(stack.popMax()); #
+print(stack.popMax()); #
 
