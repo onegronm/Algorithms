08857372c88d4cb738477e9dd08656a7e86a2a97
Omar Negron <omarnegron92@hotmail.com>
2020-12-21 20:31:47 -0500

    shuffle linked list

--- Python/Coursera/algorithms-part1/5 mergesort/shuffleLinkedList.py
+++ Python/Coursera/algorithms-part1/5 mergesort/shuffleLinkedList.py
@@ -1,9 +1,14 @@
 # Given a singly-linked list containing n items, rearrange the items uniformly at random. Your algorithm should consume a logarithmic (or constant) amount of extra memory and run in time proportional to n log n in the worst case. 
 # difficulty: hard
+# related topics: shuffling, linked lists
 # https://github.com/jsong00505/CodingStudy/blob/master/coursera/algorithms/part1/week3/mergesort/shuffling_linked_list.py
 import random
 
 class LinkedList(object):
+
+	def __init__(self):
+		self.val = 0
+		self.next = None
 
 	def __init__(self, x):
 		self.val = x
@@ -32,5 +37,33 @@ class ShufflingLinkedList:
 		right = shuffle(right)
 
 		return merge(left, right)
+
+	def merge(self, left, right):
+
+		aux = LinkedList()
+		l = left
+		r = right
+		current = aux
+
+		while l and r:
+			rand = random.randint(0,1)
+
+			if rand == 0:
+				current.next = l
+				current = current.next
+				l = l.next
+			else:
+				current.next = r
+				current = current.next
+				r = r.next
+
+		# exhausted l or r
+		if l:
+			current.next = l # If the left side is not traversed, connect it to current
+		elif r:
+			current.next = r # If the right side is not traversed, connect it to current
+
+		return aux.next # Return the merged list
+
 
 		
